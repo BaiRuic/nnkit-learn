237668b26e02a82f37c6ba86b6c997d106078a08
bairuic <brc730@outlook.com>
2023-03-18 19:39:21 +0800

    完善多种数据类型

--- nklearn/cppsrc/ndarray_backend_cpu.cc
+++ nklearn/cppsrc/ndarray_backend_cpu.cc
@@ -7,20 +7,18 @@ namespace nklearn{
 
 #define ALIGNMENT 256
 #define TILE 8
-typedef float scalar_t;
-const size_t ELEM_SIZE = sizeof(scalar_t);
 
-
+template<typename T>
 class AlignedArray{
 public:
     size_t size;
-    scalar_t * ptr;
+    T * ptr;
 
     AlignedArray(const size_t size){
-        int ret = posix_memalign((void**)&ptr, ALIGNMENT, size * ELEM_SIZE);
+        int ret = posix_memalign((void**)&ptr, ALIGNMENT, size * sizeof(T));
         if (ret == 1)
             throw std::bad_alloc();
-        this->size = size;
+        this->size = size; 
     }
 
     ~AlignedArray(){
@@ -33,6 +31,40 @@ public:
 };
 
 
+namespace py = pybind11;
+
+template<typename T>
+void from_numpy(py::array_t<T> a, AlignedArray<T> * out){
+    std::memcpy(out->ptr, a.request().ptr, out->size * sizeof(T));
+}
+
+template<typename T>
+void from_pylist(std::vector<T> a, AlignedArray<T> * out){
+    std::memcpy(out->ptr, a.data(), out->size * sizeof(T));
+}
+
+
+template<typename T>
+void declare_array(py::module &m, const std::string &typestr) {
+    using Class = AlignedArray<T>;
+    std::string pyclass_name = std::string("Array_") + typestr;
+    py::class_<Class>(m, pyclass_name.c_str())
+        .def(py::init<size_t>(), py::return_value_policy::take_ownership)
+        .def("ptr", &Class::ptr_as_int)
+        .def_readonly("size", &Class::size);
+}
+
+template<typename T>
+py::array_t<T> to_numpy(const AlignedArray<T>& a,
+                        std::vector<size_t> shape, 
+                        std::vector<size_t> strides, size_t offset){
+    std::vector<size_t> real_strides = strides;
+    std::transform(real_strides.begin(), real_strides.end(), real_strides.begin(),
+        [](size_t& c) { return c * sizeof(T); }); 
+ 
+    return py::array_t<T>(shape, real_strides, a.ptr+offset); 
+}
+
 } //namespace cpu
 } // namespace nklearn
 
@@ -44,28 +76,52 @@ PYBIND11_MODULE(ndarray_backend_cpu, m){
     m.attr("__device_name__") = "cpu";
     m.attr("__tile_size__") = TILE;
     
-    py::class_<AlignedArray>(m, "Array")
-        .def(py::init<size_t>(), py::return_value_policy::take_ownership)
-        .def("ptr", &AlignedArray::ptr_as_int)
-        .def_readonly("size", &AlignedArray::size);
+    // 暴露多种类型Array, 类名为 Array_float32
+    //declare_array<_Float16>(m, "float16");
+    declare_array<_Float32>(m, "float32");
+    declare_array<_Float64>(m, "float64");
+    declare_array<int8_t>(m, "int8");
+    declare_array<int16_t>(m, "int16");
+    declare_array<int32_t>(m, "int32");
+    declare_array<int64_t>(m, "int64");
+    declare_array<uint8_t>(m, "uint8");
+    declare_array<uint16_t>(m, "uint16");
+    declare_array<uint32_t>(m, "uint32");
+    declare_array<uint64_t>(m, "uint64");
     
+    //m.def("from_numpy", from_numpy<_Float16>);
+    m.def("from_numpy", from_numpy<_Float32>);
+    m.def("from_numpy", from_numpy<_Float64>);
+    m.def("from_numpy", from_numpy<int8_t>);
+    m.def("from_numpy", from_numpy<int16_t>);
+    m.def("from_numpy", from_numpy<int32_t>);
+    m.def("from_numpy", from_numpy<int64_t>);
+    m.def("from_numpy", from_numpy<uint8_t>);
+    m.def("from_numpy", from_numpy<uint16_t>);
+    m.def("from_numpy", from_numpy<uint32_t>);
+    m.def("from_numpy", from_numpy<uint64_t>);
 
-    m.def("from_numpy", [](py::array_t<scalar_t> a, AlignedArray * out){
-        std::memcpy(out->ptr, a.request().ptr, out->size * ELEM_SIZE);
-    });
+    //m.def("from_pylist", from_pylist<_Float16>);
+    m.def("from_pylist", from_pylist<_Float32>);
+    m.def("from_pylist", from_pylist<_Float64>);
+    m.def("from_pylist", from_pylist<int8_t>);
+    m.def("from_pylist", from_pylist<int16_t>);
+    m.def("from_pylist", from_pylist<int32_t>);
+    m.def("from_pylist", from_pylist<int64_t>);
+    m.def("from_pylist", from_pylist<uint8_t>);
+    m.def("from_pylist", from_pylist<uint16_t>);
+    m.def("from_pylist", from_pylist<uint32_t>);
+    m.def("from_pylist", from_pylist<uint64_t>);
 
-
-    m.def("from_pylist", [](py::list a, AlignedArray *out){
-        std::memcpy(out->ptr, a.cast<std::vector<scalar_t>>().data(), out->size);
-    });
-
-    m.def("to_numpy", [](const AlignedArray& a, std::vector<size_t> shape, 
-                        std::vector<size_t> strides, size_t offset){
-                            std::vector<size_t> real_strides = strides;
-                            std::transform(real_strides.begin(), real_strides.end(), real_strides.begin(),
-                                            [](size_t& c) { return c * ELEM_SIZE; }); 
-                                            
-                            return py::array_t<scalar_t>(shape, real_strides, a.ptr+offset);
-                        });
-    
+    //m.def("to_numpy", to_numpy<_Float16>);
+    m.def("to_numpy", to_numpy<_Float32>);
+    m.def("to_numpy", to_numpy<_Float64>);
+    m.def("to_numpy", to_numpy<int8_t>);
+    m.def("to_numpy", to_numpy<int16_t>);
+    m.def("to_numpy", to_numpy<int32_t>);
+    m.def("to_numpy", to_numpy<int64_t>);
+    m.def("to_numpy", to_numpy<uint8_t>);
+    m.def("to_numpy", to_numpy<uint16_t>);
+    m.def("to_numpy", to_numpy<uint32_t>);
+    m.def("to_numpy", to_numpy<uint64_t>);
 }
--- nklearn/ndarray/ndarray.py
+++ nklearn/ndarray/ndarray.py
@@ -1,6 +1,29 @@
 from . import ndarray_backend_cpu
 import numpy as np
 import math
+from enum import Enum
+import numbers
+
+def is_numeric_list(lst):
+    for el in lst:
+        if isinstance(el, list):
+            if not is_numeric_list(el):
+                return False
+        elif not isinstance(el, numbers.Number):
+            return False
+    return True
+
+
+def flatten(lst):
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+
+
 
 class BackendDevice:
     def __init__(self, name, mod):
@@ -18,7 +41,14 @@ class BackendDevice:
     
     def enableed(self):
         return self.mod is not None
-    
+
+dtypes = set(["int8", "int16", "int32", "int64", 
+            "uint8", "uint16", "uint32", "uint64",
+            "float32", "float64"])
+
+
+def default_dtype():
+    return 'float32'
 
 def cpu():
     "Return cpu device"
@@ -28,22 +58,55 @@ def default_device():
     return cpu()
 
 class NDArray:
-    def __init__(self, other, device=None):
+    def __init__(self, other, device=None, dtype=None):
+        if dtype not in dtypes:
+            raise TypeError('Could not find %s in dtypes' % (dtype))
+
         if isinstance(other, NDArray):
             if device is None:
                 device = other.device
-            self._init(other.to(device) + 0.0) # deep copy
+            if dtype is None:
+                dtype = other.dtype
+            self._init(other.to(device) + 0) # deep copy
 
         elif isinstance(other, np.ndarray):
             device = device if device else default_device()
-            array = self.make(shape=other.shape, device=device)
+            if dtype:
+                other = other.astype(dtype)
+            dtype = str(other.dtype)
+            array = self.make(shape=other.shape, device=device, dtype=dtype)
             array._device.from_numpy(np.ascontiguousarray(other), array._handle)
             self._init(array)
             
         elif isinstance(other, list):
-            pass
+            """  目前无法应对不均匀数组以及浮点型到整形的存储问题。
+            device = device if device else default_device()
+            dtype = dtype if dtype else default_dtype()
+
+            if not is_numeric_list(other):
+                raise ValueError("Invalid item in list! The item in list must be numeric")
+            # calculate the shape of list
+            cur_shape = []
+            cur_lst = other
+            while isinstance(cur_lst, list):
+                cur_shape.append(len(cur_lst))
+                cur_lst = cur_lst[0]
+            cur_shape = tuple(cur_shape)
+
+            array = self.make(shape=cur_shape, device=device, dtype=dtype)
+            # Flatten the list
+            other = flatten(other)
+            array._device.from_pylist(other, array._handle)
+            self._init(array)
+            """
+
+            try:
+                array = NDArray(np.array(other, dtype=dtype), device=device, dtype=dtype)
+                self._init(array)
+            except ValueError as r:
+                raise r
         else:
-            array = NDArray(np.array(other), device=device)
+            array = NDArray(np.array(other), device=device, dtype=dtype)
             self._init(array)
 
     def _init(self, other):
@@ -51,6 +114,7 @@ class NDArray:
         self._strides = other._strides
         self._offset = other._offset
         self._device = other._device
+        self._dtype = other._dtype
         self._handle = other._handle
 
     @staticmethod
@@ -64,7 +128,7 @@ class NDArray:
         return tuple(res[::-1])
     
     @staticmethod
-    def make(shape, strides=None, offset=0, device=None, handle=None):
+    def make(shape, strides=None, offset=0, device=None, dtype=None, handle=None):
         """Create a new NDArray with the given properties.  
         This will allocation the memory if handle=None, otherwise it will 
         use the handle of an existing array."""
@@ -73,16 +137,29 @@ class NDArray:
         array._strides = strides if strides else NDArray.compact_strides(shape)
         array._offset = offset
         array._device = device if device else default_device()
+        array._dtype = dtype if dtype else default_dtype()
         if handle:
             array._handle = handle
         else:
-            array._handle = array._device.Array(math.prod(shape))
+            array._handle = getattr(array._device, "Array_" + array._dtype)(math.prod(shape))
         
         return array
     
     def __repr__(self) -> str:
-        return "NDArray(" + self.numpy().__str__() + f", device={self._device})"
+        return "NDArray(" + self.numpy().__str__() + f", device={self._device}, dtype={self._dtype})"
     
     def numpy(self):
         return self._device.to_numpy(self._handle, self._shape, self._strides, self._offset)
     
+    def to(self, device):
+        if device == self._device:
+            return self
+        else:
+            return NDArray(self.numpy(), dtype=self.dtype, sdevice=device)
+    
+    @property
+    def dtype(self):
+        return self._dtype
+
+    def astype(self, dtype):
+        return NDArray(self.numpy(), device=self._device, dtype=dtype)
